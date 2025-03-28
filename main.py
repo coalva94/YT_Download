f777bacaca984457a7ee044edb296489e31968cc
Carlos <coalva.94@gmail.com>
2025-03-28 16:00:43 -0500

    subida inicial del proyecto

---
+++ main.py
@@ -0,0 +1,154 @@
+import yt_dlp
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+import os
+
+
+# Crear la ventana principal
+root = tk.Tk()
+root.title("YouTube Downloader")
+root.geometry("410x600")
+root.resizable(False, False)
+ttk.Label(root, text="Descarga audios o videos de Youtube:").pack()
+
+
+# Funciones
+#Explorador de carpetas
+selected_folder = os.getcwd()
+def select_folder():
+    global selected_folder
+    selected_folder = filedialog.askdirectory()
+    if selected_folder:
+        path_entry.delete(0, tk.END)
+        path_entry.insert(0, selected_folder)
+        path_entry.configure(foreground="black") 
+                
+        
+ # Diccionario para convertir resoluciones de "ancho x alto" a estándar (360p, 720p, etc.)
+resolution_map = {
+    "3840x2160": "2160p",
+    "2560x1440": "1440p",
+    "1920x1080": "1080p",
+    "1280x720": "720p",
+    "854x480": "480p",
+}       
+   
+        
+selected_format_id = tk.StringVar(value='')
+
+               
+def list_formats(url):
+    """Muestra una lista filtrada de formatos estándar sin repetidos y permite descargar"""
+    try:
+        with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
+            info = ydl.extract_info(url, download=False)  # Obtener info sin descargar
+            formats = info.get('formats', [])
+
+            format_dict = {}  # Diccionario para evitar resoluciones repetidas
+
+            for f in formats:
+                raw_resolution = f.get('resolution', 'Desconocida')  # Ej. "640x360"
+                standard_resolution = resolution_map.get(raw_resolution, raw_resolution)  # Convertir a 360p, 720p...
+
+                ext = f['ext']  # Obtener formato (mp4, webm, etc.)
+                format_id = f['format_id']  # ID del formato
+
+                # Solo guardar una versión por resolución (priorizando mp4)
+                if standard_resolution not in format_dict or ext == 'mp4':
+                    format_dict[standard_resolution] = (format_id, ext)
+
+            # Ordenar según calidad (de mayor a menor)
+            quality_order = ["2160p", "1440p", "1080p", "720p", "480p"]
+            available_formats = {}
+            radio = ttk.Frame(root)   
+            radio.pack(anchor="center", padx=2, pady=5)
+            ttk.Label(radio, text="Elije la calidad:").pack(pady=5) 
+            for resolution in quality_order:
+                if resolution in format_dict:
+                    format_id, ext = format_dict[resolution]
+                    available_formats[format_id] = resolution  # Guardar formato disponible
+                    print(f"{format_id} | {ext}  | {resolution}")                                             
+                    ttk.Radiobutton(radio, text=resolution, variable=selected_format_id, value=format_id).pack(side='left', padx = 5)     
+            return available_formats # Retornar los formatos disponibles
+
+    except Exception:
+        ttk.Label(root, text="❌ Error al obtener formatos").pack(pady=5)     
+        return None        
+
+               
+# Descargar solo audio
+def descargar_audio(url, folder):
+    """Descarga el audio"""
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }],
+        'outtmpl': os.path.join(folder, '%(title)s.%(ext)s'),
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            print("✅ Descarga completada")
+
+    except Exception as e:
+        print(f"❌ Error al descargar el audio: {e}")
+        return None
+    
+    
+# Botón para iniciar la descarga (video)
+def button_download():
+    download_button = tk.Button(root, text="Descargar video", command=lambda: download_video(url_entry.get(), selected_format_id.get(), selected_folder ))
+    download_button.pack(pady=5)
+    
+      
+def download_video(url, format_id, folder):
+    """Descarga el video con el formato seleccionado"""
+    ydl_opts = {
+        'format': f'{format_id}+ba/b',
+        'merge_output_format': 'mp4',
+        'outtmpl': os.path.join(folder, '%(title)s.%(ext)s'),
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+    except Exception as e:
+        print(f"❌ Error al descargar el video:{e} ")   
+   
+  
+
+# Label para link
+ttk.Label(root, text="Ingrese el enlace:").pack(pady=5)
+url_entry = ttk.Entry(root, width=80)
+url_entry.pack(padx=2)
+
+
+# Botón para abrir explorador de carpetas 
+carpeta = ttk.Frame(root)
+carpeta.pack(anchor="center", pady=5, padx = 2)
+ttk.Label(carpeta, text="Ubicacion del archivo:").pack(pady=5)
+path_entry = ttk.Entry(carpeta, width=37)
+path_entry.pack(side = 'left')
+path_entry.insert(0, selected_folder)
+path_entry.configure(foreground="gray") 
+ttk.Button(carpeta, text = '📂',command=select_folder).pack(side = 'left', pady = 5)
+
+
+#Botones de calidades (video) y descarga (audio)
+botones = ttk.Frame(root)
+botones.pack(anchor="center")
+# Crear el botón inicial
+ttk.Button(botones, text="Calidades disponibles (videos)", command=lambda:(list_formats(url_entry.get()), button_download())).pack(side = 'left')
+# Descargar solo audio
+ttk.Button(botones, text="Descargar solo audio", command=lambda:descargar_audio(url_entry.get(),selected_folder)).pack(side = 'left')
+
+
+root.mainloop()
+
+
+# https://www.youtube.com/watch?v=5KTlHuNTwQA
